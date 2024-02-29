a46c004c021c9ed4a999b08dbeb366d96fd1a396
UsamaAhmadAwan <usama786760@gmail.com>
2024-02-29 17:48:32 +0500

    learning params

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@
 #   root 'homepages#homepage'
 # end
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, only: [:show]
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'show', to: 'articles#show'
 end
