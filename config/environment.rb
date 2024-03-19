f7b6b964ffb63fc64ca5d400bd55349510281de2
AfaqShahidKhan <afaqk3203@gmail.com>
2024-03-19 14:18:26 +0500

    conflict resolve

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-ActionView::base.field_error_proc = proc.new do |html_tag, instance|
-    html_tag.html_safe
+# Fix field error rendering
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
 end
